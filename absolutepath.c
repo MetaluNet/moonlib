fded5a9fe998129f7326a36919305244012c37fa
Antoine Rousseau <antoine@metalu.net>
2015-12-17 19:25:06 +0100

    Fix absolutepath once again...

--- absolutepath.c
+++ absolutepath.c
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 //extern t_canvas *pd_this->pd_canvaslist;	    	    /* list of all root canvases */
 //#ifdef pd_this
-#if (PD_MAJOR_VERSION == 0) && (PD_MINOR_VERSION < 46)
+#if ((PD_MAJOR_VERSION == 0) && (PD_MINOR_VERSION < 46))
 extern t_canvas *canvas_list;
 #else
 #define canvas_list pd_this->pd_canvaslist
@@ -130,7 +130,7 @@ static void absolutepath_symbol(t_absolutepath *x,t_symbol *sym)
 static void *absolutepath_new(t_symbol *s,int argc,t_atom *argv)
 {
     t_absolutepath *x = (t_absolutepath *)pd_new(absolutepath_class);
-    t_canvas *can=pd_this->pd_canvaslist;
+    t_canvas *can=canvas_list;
     int d0;
 
     outlet_new(&x->x_obj, 0);
