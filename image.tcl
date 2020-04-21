d88312148fddc83599273f7557e3631f8c1a223b
Antoine Rousseau <_antoine_@metalu.net>
2020-04-21 19:20:15 +0200

    image: fix Tcl hanging when trying to open unrecognized image file

--- image.tcl
+++ image.tcl
@@ -4,13 +4,13 @@ namespace eval ::moonlib::image:: {
 
 proc ::moonlib::image::configure {obj_id img filename} {
     if { [catch {$img configure -file $filename} fid]} {
-        ::pdwindow::logpost $obj_id 1 "[image]: error reading '$filename':\n$fid\n"
+        ::pdwindow::logpost $obj_id 1 "image: error reading '$filename':\n$fid\n"
     }
 }
 
 proc ::moonlib::image::create_photo {obj_id img filename} {
     if { [catch {image create photo $img -file $filename} fid]} {
-        ::pdwindow::logpost $obj_id 1 "[image]: error reading '$filename':\n$fid\n"
+        ::pdwindow::logpost $obj_id 1 "image: error reading '$filename':\n$fid\n"
     }
 }
 
