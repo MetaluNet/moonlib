105b8da86283b9d581dc23fe8ac2cc93f852e514
Antoine Rousseau <_antoine_@metalu.net>
2026-05-08 11:40:33 +0200

    fix iemgui_static

--- iemgui_static.h
+++ iemgui_static.h
@@ -166,7 +166,7 @@ static int s_pd_snprintf(char *buf, size_t size, const char *fmt, ...)
     int ret;
     va_list ap;
     va_start(ap, fmt);
-    ret = pd_vsnprintf(buf, size, fmt, ap);
+    ret = s_pd_vsnprintf(buf, size, fmt, ap);
     va_end(ap);
     return ret;
 }
